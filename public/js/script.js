fcaa8ef274b7a2fc65a2318fbb8a5f8dc9304264
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-09-05 00:16:15 +0200

    SCRIOT: HEADER che compare allo scroll sopra un certo viewport

--- public/js/script.js
+++ public/js/script.js
@@ -10707,9 +10707,24 @@ $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 function init() {
   console.log("Hello World");
+  $(window).on("scroll", headerScroll);
 }
 
 $(document).ready(init);
+/* Funzione per azione scroll, far apparire header */
+
+function headerScroll() {
+  var heigh_viewport = $(window).height();
+  var height_show_header = heigh_viewport * 60 / 100;
+  /* console.log("ALTEZZA VIEWPORT:", heigh_viewport);
+  console.log("THIS SCROLL:", $(window).scrollTop()); */
+
+  if ($(window).scrollTop() > height_show_header) {
+    $('.hidden_header').addClass("block");
+  } else {
+    $('.hidden_header').removeClass("block");
+  }
+}
 
 /***/ }),
 
--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,31 @@ $ = require("jquery");
 
 function init() {
     console.log("Hello World");
+
+    $(window).on("scroll", headerScroll);
 }
 
 $(document).ready(init);
+
+/* Funzione per azione scroll, far apparire header */
+function headerScroll(){
+
+    
+    var heigh_viewport = $(window).height();
+    var height_show_header = (heigh_viewport * 60) / 100;
+    /* console.log("ALTEZZA VIEWPORT:", heigh_viewport);
+    console.log("THIS SCROLL:", $(window).scrollTop()); */
+    
+
+
+    if ($(window).scrollTop() > height_show_header) {
+        
+        $('.hidden_header').addClass("block");
+        
+    }else {
+        $('.hidden_header').removeClass("block");
+        
+    }
+
+   
+}
